c44ae44ae54396b3374889149e275e2342d7a700
paolacorredormt <paola.corredor.mt@correo.usa.edu.co>
2021-10-18 00:25:33 -0500

    first commit

---
+++ JS/misFunciones.js
@@ -0,0 +1,11 @@
+function traerInformacionCategorias(){
+    $.ajax({
+        url:"http://localhost:8080/api/Category/all",
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            console.log(respuesta);
+            pintarRespuesta(respuesta);
+        }
+    })
+}
